0909dc76a32ebdcadfa6a04c5c37446488b59956
Laszlo Heves <heves.laszlo@gmail.com>
2019-08-03 20:49:11 +0100

    getFeaturesFromCookie test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,14 +1,11 @@
 jest.mock('js-cookie');
-const {featureService} = require('../index');
+const { featureService } = require('../index');
 const Cookies = require("js-cookie");
 
 describe('setFeatureCookie', () => {
   test('should call cookie.set with `true` as value', () => {
     const param = 'foo';
     featureService.setFeatureCookie(param);
-    
-    
-    
     expect(Cookies.set).toHaveBeenCalledWith(param, true)
   });
 
@@ -56,4 +53,32 @@ describe('getFeaturesFromUrl', () => {
     expect(featureService.setFeatureCookie).toHaveBeenCalledWith(queryParam1);
     expect(featureService.setFeatureCookie).toHaveBeenCalledWith(queryParam2);
   })
+});
+
+describe('getFeaturesFromCookie', () => {
+  beforeEach(() => {
+    document.cookie = "fake=aaa;expires='Wed, 31 Oct 2012 08:50:17 GMT'"
+    document.cookie = "f_legit=bbb;expires='Wed, 31 Oct 2012 08:50:17 GMT'"
+    document.cookie = "f_legit2=ccc;expires='Wed, 31 Oct 2012 08:50:17 GMT'"
+    document.cookie = "f_falsy=false;expires='Wed, 31 Oct 2012 08:50:17 GMT'"
+    document.cookie = "f_truthy=true;expires='Wed, 31 Oct 2012 08:50:17 GMT'"
+  });
+  test('return empty object if no cookie is set', () => {
+    document.cookie = "";
+    expect(featureService.getFeaturesFromCookie()).toEqual({});
+  });
+
+  test('return feature object from cookie', () => {
+    document.cookie = "fake=aaa";
+    document.cookie = "f_legit=bbb";
+    document.cookie = "f_legit2=ccc";
+    expect(featureService.getFeaturesFromCookie()).toEqual({ legit: 'bbb', legit2: 'ccc' });
+  });
+
+  test('return with boolean value', () => {
+    document.cookie = "f_falsy=false";
+    document.cookie = "f_truthy=true";
+    expect(featureService.getFeaturesFromCookie()).toEqual({ falsy: false, truthy: true });
+  });
+
 });
